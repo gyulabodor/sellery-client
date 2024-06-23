5fdaf667ba356e85f4c48334b23ea1d1c2da8b27
Bodor Gyula <bodor.gyula@novaservices.hu>
2024-06-24 01:57:15 +0200

    implement svg registration into ngOnInit() in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IconRegistryService} from "./template/icons/icon-registry.service";
+import {IconList} from "./template/icons/icon-list";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Sellery';
+
+  constructor(private iconRegister: IconRegistryService) {}
+
+  ngOnInit() {
+    this.iconRegister.registerSvgIcons(IconList);
+  }
 }

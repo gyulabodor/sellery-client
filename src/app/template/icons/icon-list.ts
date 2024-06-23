bf88ce27cff9c405aa0064833beddc76d564a1d4
Bodor Gyula <bodor.gyula@novaservices.hu>
2024-06-24 01:50:37 +0200

    create icon-list.ts and add svg icons to the array

---
+++ src/app/template/icons/icon-list.ts
@@ -0,0 +1,12 @@
+import {IconData} from "./icon.model";
+
+export const IconList: IconData[] = [
+  {
+    name: "play_arrow",
+    path: "assets/svg/play_arrow_icon.svg",
+  },
+  {
+    name: "double_arrow_down",
+    path: "assets/svg/double_arrow_down_icon.svg"
+  }
+]
